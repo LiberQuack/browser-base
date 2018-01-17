7b465156680ccb90ae7e0a1e8a0beeb8d526cc7b
Thiago Martins <rogue.thiago@gmail.com>
2018-01-17 01:55:55 -0200

    Added option for overwritting framerate

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,10 @@
 
 RECORDED_FILE_NAME=${CUSTOM_RECORD_NAME:-`cat /etc/browser || echo 'record'`}
 VIDEO_EXTENSION=${VIDEO_EXTENSION:-'mkv'}
+FRAME_RATE=${VIDEO_EXTENSION:-'30'}
 
 Xvfb $DISPLAY -ac -screen 0 ${RESOLUTION}x24 >& /tmp/Xvfb.log &
 sleep 1 && fluxbox >& /tmp/fluxbox.log &
 sleep 1 && x11vnc >& /tmp/x11vnc.log &
-sleep 1 && ffmpeg -f x11grab -video_size $RESOLUTION -i $DISPLAY -codec:v libx264 -r 30 -y "${VIDEO_DIRECTORY}/${RECORDED_FILE_NAME}.${VIDEO_EXTENSION}" >& /tmp/ffmpeg.log &
+sleep 1 && ffmpeg -f x11grab -video_size $RESOLUTION -i $DISPLAY -codec:v libx264 -r $FRAME_RATE -y "${VIDEO_DIRECTORY}/${RECORDED_FILE_NAME}.${VIDEO_EXTENSION}" >& /tmp/ffmpeg.log &
 sleep 1 && exec "$@"
